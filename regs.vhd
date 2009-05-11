0c2ab8b2a0488a5eda6c2b3d30ff6837e4e1b540
fester3000 <fester3000@0c5c26d2-38ef-11de-a996-7ff70a941223>
2009-05-11 21:51:36 +0000

    zmiana nazwy pliku z rejestrami

--- regs.vhd
+++ regs.vhd
@@ -55,3 +55,45 @@ begin
 	q <= q_reg;
 	
 end data_flow;
+
+entity reg2 is
+	port
+	(
+	
+		--INPUTS
+		--@@ TODO dodaæ stygna³y z US
+		clk : in std_logic;
+		rst : in std_logic;
+		ena : in std_logic;
+		d : in std_logic_vector ( 1 downto 0 );
+		
+
+		--OUTPUTS
+		q : out std_logic_vector ( 1 downto 0 )
+		
+	);
+end reg2;
+
+-- Library Clause(s) (optional)
+-- Use Clause(s) (optional)
+
+architecture data_flow of reg2 is
+
+	signal q_reg, q_next : std_logic_vector ( 1 downto 0 );
+
+begin
+	process(clk, rst)
+	begin
+		if(rst='1') then
+			q_reg <= (others => '0');
+		elsif rising_edge(clk) then
+			q_reg <= q_next;
+		end if;
+	end process;
+	
+	q_next <= d when ena = '1' else
+			q_reg;
+			
+	q <= q_reg;
+	
+end data_flow;
