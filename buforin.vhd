393338c7717ff5f53ee9d93c0448b8adf2187cc3
fester3000 <fester3000@0c5c26d2-38ef-11de-a996-7ff70a941223>
2009-05-11 21:04:45 +0000

    próba połączenia tego szitu

--- buforin.vhd
+++ buforin.vhd
@@ -24,7 +24,8 @@ entity buforin is
 		clk : in  std_logic ;
 		rst : in std_logic;
 		data  : in std_logic_vector ( 7 downto 0 );
-		
+		enable : in std_logic;
+		sel : in std_logic_vector ( 1 downto 0 );
 			--OUTPUTS
 		usb_endread : out std_logic;
 		index : out std_logic_vector ( 1 downto 0 );
@@ -40,25 +41,100 @@ end buforin;
 architecture data_flow of buforin is
 
 
---component dmux4x8	
---	port
---	(
---		input			: in std_logic_vector ( 7 downto 0 );
---		sel				: in std_logic_vector( 1 downto 0 );
---		o1, o2, o3, o4	: out std_logic_vector ( 7 downto 0 )
---		
---	);
---end component;	
+component dmux4x8	
+	port
+	(
+		input			: in std_logic_vector ( 7 downto 0 );
+		sel				: in std_logic_vector( 1 downto 0 );
+		o1, o2, o3, o4	: out std_logic_vector ( 7 downto 0 )
+		
+	);
+end component;	
 
+component mux4x8
+	port(
+		i1, i2, i3, i4	: in std_logic_vector ( 7 downto 0 );
+		sel				: in std_logic_vector ( 1 downto 0 );
+		output 			: out std_logic_vector ( 7 downto 0 )
+	);
+end component;
+
+component reg8
+	port
+	(
+		--INPUTS
+		--@@ TODO dodaæ stygna³y z US
+		clk : in std_logic;
+		rst : in std_logic;
+		ena : in std_logic;
+		d : in std_logic_vector ( 7 downto 0 );
+		
+
+		--OUTPUTS
+		q : out std_logic_vector ( 7 downto 0 )
+		
+		
+	);
+end component;
+
+signal sig1_a, sig1_b, sig1_c, sig1_d, sig2_a, sig2_b, sig2_c, sig2_d : std_logic_vector ( 7 downto 0 );
 begin
+	reg_0 : reg8
+		port map ( 
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_a,
+q => sig2_a
+);
 
---	dmux1 : dmux4x8
---		port map (
---		
---		);
---	dmux2 : dmux2x8 
---		port map (
---			
---		);
+	reg_1 : reg8
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_b,
+q => sig2_b
+);
+
+	reg_2 : reg8
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_c,
+q => sig2_c
+);
+
+	reg_3 : reg8
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_d,
+q => sig2_d
+);
+
+
+	dmux1 : dmux4x8
+		port map (
+			input => data,
+			sel => sel,
+			o1 => sig1_a,
+			o2 => sig1_b,
+			o3 => sig1_c,
+			o4 => sig1_d
+		);
+		
+
+	mux2 : mux4x8 
+		port map (
+				output => data_index,
+			sel => sel,
+			i1 => sig2_a,
+			i2 => sig2_b,
+			i3 => sig2_c,
+			i4 => sig2_d
+		);
 
 end data_flow;
