f8a5c52cde3b8f4d0203a9859543bbb2850a36eb
maku17 <maku17@0c5c26d2-38ef-11de-a996-7ff70a941223>
2009-05-12 18:33:04 +0000

    dodanie rejestrów rdm, status

--- buforin.vhd
+++ buforin.vhd
@@ -40,7 +40,6 @@ end buforin;
 
 architecture data_flow of buforin is
 
-
 component dmux4x8	
 	port
 	(
@@ -78,6 +77,48 @@ component reg8
 end component;
 
 signal sig1_a, sig1_b, sig1_c, sig1_d, sig2_a, sig2_b, sig2_c, sig2_d : std_logic_vector ( 7 downto 0 );
+
+-- dmuxy i muxy odpowiedzialne za rdm
+
+component dmux4x32	
+	port
+	(
+		input			: in std_logic_vector ( 31 downto 0 );
+		sel				: in std_logic_vector( 1 downto 0 );
+		o1, o2, o3, o4	: out std_logic_vector ( 31 downto 0 )
+		
+	);
+end component;	
+
+component mux4x32
+	port(
+		i1, i2, i3, i4	: in std_logic_vector ( 31 downto 0 );
+		sel				: in std_logic_vector ( 1 downto 0 );
+		output 			: out std_logic_vector ( 31 downto 0 )
+	);
+end component;
+
+component reg32
+	port
+	(
+		--INPUTS
+		--@@ TODO dodaæ stygna³y z US
+		clk : in std_logic;
+		rst : in std_logic;
+		ena : in std_logic;
+		d : in std_logic_vector ( 31 downto 0 );
+		
+
+		--OUTPUTS
+		q : out std_logic_vector ( 31 downto 0 )
+		
+		
+	);
+end component;
+-- sygna³y do rdm
+signal sig1_a_rdm, sig1_b_rdm, sig1_c_rdm, sig1_d_rdm, sig2_a_rdm, sig2_b_rdm, sig2_c_rdm, sig2_d_rdm : std_logic_vector ( 31 downto 0 );
+
+
 begin
 	reg_0 : reg8
 		port map ( 
@@ -136,5 +177,72 @@ q => sig2_d
 			i3 => sig2_c,
 			i4 => sig2_d
 		);
+
+
+-- rejestry RDM ka¿dy 32bit 
+
+rdm_0 : reg32
+		port map ( 
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_a_rdm,
+q => sig2_a_rdm
+);
+
+	rdm_1 : reg32
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_b_rdm,
+q => sig2_b_rdm
+);
+
+	rdm_2 : reg32
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_c_rdm,
+q => sig2_c_rdm
+);
+
+	rdm_3 : reg32
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_d_rdm,
+q => sig2_d_rdm
+);
+
+
+	dmux1_rdm : dmux4x32
+		port map (
+			input => data,
+			sel => sel,
+			o1 => sig1_a_rdm,
+			o2 => sig1_b_rdm,
+			o3 => sig1_c_rdm,
+			o4 => sig1_d_rdm
+		);
+		
+
+	mux2_rdm : mux4x32 
+		port map (
+				output => data_index,
+			sel => sel,
+			i1 => sig2_a_rdm,
+			i2 => sig2_b_rdm,
+			i3 => sig2_c_rdm,
+			i4 => sig2_d_rdm
+		);
+
+
+
+
+
+
 
 end data_flow;
--- buforout.vhd
+++ buforout.vhd
@@ -37,59 +37,109 @@ end buforout;
 
 architecture data_flow of buforout is
 
-	signal stat1, stat2, stat3, stat4, statout : std_logic_vector ( 1 downto 0 );
-	signal sel : std_logic_vector ( 1 downto 0 );
-	signal status : std_logic_vector ( 1 downto 0 );
+--	signal stat1, stat2, stat3, stat4, statout : std_logic_vector ( 1 downto 0 );
+--	signal sel : std_logic_vector ( 1 downto 0 );
+--	signal status : std_logic_vector ( 1 downto 0 );
 
---component reg2 is
---	port
---	(
---	
---		--INPUTS
---		--@@ TODO dodaæ stygna³y z US
---		clk : in std_logic;
---		rst : in std_logic;
---		ena : in std_logic;
---		d : in std_logic_vector ( 1 downto 0 );
---		
---
---		--OUTPUTS
---		q : out std_logic_vector ( 1 downto 0 )
---		
---	);
---end component;
---
---component dmux4x2 is
---	port(
---		input			: in std_logic_vector ( 1 downto 0 );
---		sel				: in std_logic_vector( 1 downto 0 );
---		o1, o2, o3, o4	: out std_logic_vector ( 1 downto 0 )
---	);
---end component;
---
 
+--- rejestry w buforout. przechowuj¹ce statusy crc, 
+
+component dmux4x2	
+	port
+	(
+		input			: in std_logic_vector ( 1 downto 0 );
+		sel				: in std_logic_vector( 1 downto 0 );
+		o1, o2, o3, o4	: out std_logic_vector ( 1 downto 0 )
+		
+	);
+end component;	
+
+component mux4x2
+	port(
+		i1, i2, i3, i4	: in std_logic_vector ( 1 downto 0 );
+		sel				: in std_logic_vector ( 1 downto 0 );
+		output 			: out std_logic_vector ( 1 downto 0 )
+	);
+end component;
+
+component reg2
+	port
+	(
+		--INPUTS
+		--@@ TODO dodaæ stygna³y z US
+		clk : in std_logic;
+		rst : in std_logic;
+		ena : in std_logic;
+		d : in std_logic_vector ( 1 downto 0 );
+		
+
+		--OUTPUTS
+		q : out std_logic_vector ( 1 downto 0 )
+		
+		
+	);
+end component;
+
+signal sig1_a, sig1_b, sig1_c, sig1_d, sig2_a, sig2_b, sig2_c, sig2_d : std_logic_vector ( 1 downto 0 );
 begin
---	reg_1: reg2
---		port map (
---			clk => clk,
---			rst => rst,
---			ena => ena,
---			d => stat1,
---			q => statout
---		);
---
---
---	dmux_2: dmux4x2 
---		port map (
---		
---			input => status,
---			o1 => stat1,
---			o2 => stat2,
---			o3 => stat3,
---			o4 => stat4,
---			
---			sel => sel		
---		);
---	
+	status_0 : reg2
+		port map ( 
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_a,
+q => sig2_a
+);
+
+	status_1 : reg2
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_b,
+q => sig2_b
+);
+
+	status_2 : reg2
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_c,
+q => sig2_c
+);
+
+	status_3 : reg2
+		port map (
+clk => clk,
+rst => rst,
+ena => enable,
+d => sig1_d,
+q => sig2_d
+);
+
+
+	dmux1 : dmux4x2
+		port map (
+			input => data,
+			sel => sel,
+			o1 => sig1_a,
+			o2 => sig1_b,
+			o3 => sig1_c,
+			o4 => sig1_d
+		);
+		
+
+	mux2 : mux4x2 
+		port map (
+				output => data_index,
+			sel => sel,
+			i1 => sig2_a,
+			i2 => sig2_b,
+			i3 => sig2_c,
+			i4 => sig2_d
+		);
+
+
 
 end data_flow;
